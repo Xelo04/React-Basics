db9391d490e3b703e976d9c7b4aa0b9601fcb09a
Aleksander Wanot <aleksnader.wanot@gmail.com>
2025-03-28 18:45:57 +0100

    refactor ListGroup component to use React fragments for better structure

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,34 @@
+// import { Fragment } from "react";
+
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // <h1>List</h1> u cant use more than 2 elements in a single component
+    // u can wrap elements in a single element like div or fragment using CTRL+SHIFT+P -> wrap with abbreviation
+    // <div>
+    //   <h1>List</h1>
+    //   <ul className="list-group">
+    //     <li className="list-group-item">An item</li>
+    //     <li className="list-group-item">A second item</li>
+    //     <li className="list-group-item">A third item</li>
+    //     <li className="list-group-item">A fourth item</li>
+    //     <li className="list-group-item">And a fifth one</li>
+    //   </ul>
+    // </div>
+
+    //but better way is to use fragment
+    // to use fragment u dont have to import it and wirte it, just use <> and </>
+    // <Fragment>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+    // </Fragment>
   );
 }
 
